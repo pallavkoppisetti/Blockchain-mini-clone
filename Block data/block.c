4ec2f10aaa2ae138f753dded74b56cca6cf84295
jewelben <jewel2002.ben@gmail.com>
2021-05-04 20:37:45 +0530

    Added attack function

--- Block data/block.c
+++ Block data/block.c
@@ -1,34 +1,49 @@
-#include "block.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "block.h"
 
 BlockChainTail = NULL;
 BlockChainHead = NULL;
+srand_flag = false;
+
+void __initialisesrand()
+{   
+    srand(time(0) * getpid());
+    srand_flag = true;
+}
 
 long long GenerateHashValue(BlockPtr CurrentBlock) //Placeholder hash function. PS - Very bad for now
 {
-    return CurrentBlock->Nonce * CurrentBlock->PreviousBlockHash * CurrentBlock->Block_no;
+    return CurrentBlock->Nonce * CurrentBlock->PreviousBlockHash * CurrentBlock->BlockNumber;
 }
 
-int GenerateNonce(BlockPtr CurrentBlock)
-{
+int GenerateNonce()
+{   
+    if(!srand_flag)
+        __initialisesrand();
+
     return (rand() % 50 + 1);
 }
 
-BlockPtr CreateNewBlock(struct Transaction TempTransactions[], BlockPtr PrevBlock)
+BlockPtr CreateNewBlock(struct Transaction TempTransactions[])
 {
     BlockPtr NewBlock = (BlockPtr)malloc(sizeof(Block)); //Might not work right now
 
-    NewBlock->Nonce = GenerateNonce(NewBlock);
+    NewBlock->Nonce = GenerateNonce();
 
     if (BlockChainHead == NULL)
     {
-        NewBlock->Block_no = 0;
+        NewBlock->BlockNumber = 1;
         NewBlock->PreviousBlockHash = 0;
     }
     else
     {
-        NewBlock->Block_no = PrevBlock->Block_no + 1;
-        NewBlock->PreviousBlockHash = PrevBlock->BlockHash;
+        NewBlock->BlockNumber = BlockChainTail->BlockNumber + 1;
+        NewBlock->PreviousBlockHash = BlockChainTail->BlockHash;
     }
 
     NewBlock->TransactionList = TempTransactions;
@@ -45,10 +60,67 @@ BlockPtr CreateNewBlock(struct Transaction TempTransactions[], BlockPtr PrevBloc
     }
     else
     {
-        PrevBlock->Next = NewBlock;
-        NewBlock->Prev = PrevBlock;
+        BlockChainTail->Next = NewBlock;
+        NewBlock->Prev = BlockChainTail;
     }
 
     NewBlock->Next = NULL;
     BlockChainTail = NewBlock;
+}
+
+void Attack()
+{   
+    if(!srand_flag)
+        __initialisesrand();
+
+    int RandomBlockNumber = (rand() % 50) + 1;
+
+    //Check if the randomly generated block number is valid
+    if(RandomBlockNumber <= BlockChainTail->BlockNumber)
+    {   
+        //Traversing from both sides reduces the time required for searching by half
+        BlockPtr LeftPtr = BlockChainHead;  
+        BlockPtr RightPtr = BlockChainTail;
+
+        //Traversing the blockchain from both sides
+        while(LeftPtr && RightPtr && LeftPtr->BlockNumber <= RightPtr->BlockNumber)
+        {
+            if(RandomBlockNumber == LeftPtr->BlockNumber)
+            {
+                int Nonce = GenerateNonce();
+
+                //To guarantee that nonce changes
+                while(Nonce == LeftPtr->Nonce)
+                {
+                    Nonce = GenerateNonce();
+                }
+                LeftPtr->Nonce = Nonce;
+                
+                printf("Attack Successfull, Nonce of Block %d successfully modified.\n", RandomBlockNumber);
+                return;
+            }
+            else if(RandomBlockNumber == RightPtr->BlockNumber)
+            {
+                int Nonce = GenerateNonce();
+
+                //To guarantee that nonce changes
+                while(Nonce == RightPtr->Nonce)
+                {
+                    Nonce = GenerateNonce();
+                }
+                LeftPtr->Nonce = Nonce;
+
+                printf("Attack Successful, Nonce of Block %d successfully modified.\n", RandomBlockNumber);
+                return;
+            }
+            else
+            {
+                LeftPtr = LeftPtr->Next;
+                RightPtr = RightPtr->Prev;
+            }
+        }
+    }
+
+    printf("Attack Failed.\n");
+    return;
 }
--- Block data/block.h
+++ Block data/block.h
@@ -6,7 +6,7 @@ typedef Block *BlockPtr;
 
 struct Block
 {
-    int Block_no;
+    int BlockNumber;
     int Nonce;
     long long BlockHash;
     long long PreviousBlockHash;
@@ -17,18 +17,26 @@ struct Block
 
 extern BlockPtr BlockChainHead; //Keeps track of the head of the blockchain
 extern BlockPtr BlockChainTail; //Keeps track of the last block in the blockchain
+extern bool srand_flag;
+
+void __initialisesrand();
+//To ensure that srand is called only once, otherwise rand() won't work properly sometimes
 
 long long GenerateHashValue(BlockPtr CurrentBlock);
 //Generates hash for the current block by using
 //the information stored in the current block
 
-BlockPtr CreateNewBlock(struct Transaction TempTransactions[], BlockPtr PrevBlock);
+BlockPtr CreateNewBlock(struct Transaction TempTransactions[]);
 //Creates a new block and adds it to the blockchain. Adds the transaction information
 //(assuming it to be stored as a struct array temporarily)
 
 int GenerateNonce();
 //Generates a random nonce value for the currently selected block;
 
-//IMP - Might need deque to account for validating the blockchain
+void Attack();
+//Generate random block number from 1 to 50,
+//If such a block exists, modify its nonce
+
+//IMP - Might need doubly linked list to account for validating the blockchain
 
 #endif
