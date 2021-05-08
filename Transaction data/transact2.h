53c2b65f52874e9a03d386a2c8b2c5783c18e3bd
jewelben <jewel2002.ben@gmail.com>
2021-05-08 18:24:50 +0530

    minor changes

--- Transaction data/transact2.h
+++ Transaction data/transact2.h
@@ -11,16 +11,14 @@ int ind;
 typedef struct Transaction Transaction;
 typedef struct User User;
 typedef struct UserTransactionHistory UserTransactionHistory;
-typedef long long int ElementType;
 typedef struct pointer pointer;
-
 
 struct Transaction
 {
     long long int SenderUID;
     long long int ReceiverUID;
     long long int AmountToBeTransferred;
-    
+
 };
 
 struct User
@@ -35,16 +33,17 @@ struct UserTransactionHistory
 {
     long long int AmountTransferred;
     long long int SenderUID;
-    UserTransactionHistory* next;
-    UserTransactionHistory* prev;
+    long long int ReceiverUID;
+    int TranscationType; // 0 -> Amount Received, 1 -> Amount Sent
+    UserTransactionHistory *next;
+    UserTransactionHistory *prev;
 };
 
 struct pointer
 {
-UserTransactionHistory* H;
-UserTransactionHistory* T;
+    UserTransactionHistory *H;
+    UserTransactionHistory *T;
 };
-
 
 User *SearchUserByID(long long int UID /*,pointer to array of pointers to users*/);
 //First search user by given ID and get pointers to the users.
@@ -55,15 +54,16 @@ int TransactionValidity(Transaction *T, User *Sender, User *Receiver);
 void UpdateUserHistory(Transaction *T, User *Sender, User *Receiver);
 // updates user transaction history (and WalletBalance)of both sender and receiver.
 
-pointer* CreateUserTransactHistory();
+pointer *CreateUserTransactHistory();
 
-void InitCreateTransactHistory(pointer* Q);
+void InitCreateTransactHistory(pointer *Q);
 
-void PrintUserTransactionHistory(UserTransactionHistory* P);
+void PrintUserTransactionHistory(UserTransactionHistory *P);
 
-void push(pointer* Q,ElementType AmountTransferred,ElementType SenderUID);
+void push(pointer *Q, long long int AmountTransferred, long long int SenderUID, long long int ReceiverUID, int TransactionType);
 //Insert item on the front end of the list.
 
-Transaction* InitializeTransactionArray();
-void UpdateBlockTransactionArray(Transaction* T,Transaction* arr);
+Transaction *InitializeTransactionArray();
+void UpdateBlockTransactionArray(Transaction *T, Transaction *arr);
+
 #endif
