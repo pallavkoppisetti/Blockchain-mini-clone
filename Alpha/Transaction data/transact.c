940309cf1e251e9736f78d1ae0835f2184cdedcd
ANMOL2212002 <82955956+ANMOL2212002@users.noreply.github.com>
2021-05-09 20:38:27 +0530

    added comments

--- Alpha/Transaction data/transact.c
+++ Alpha/Transaction data/transact.c
@@ -5,7 +5,8 @@
 int NumberofTempTransactions = 0;
 
 void InitCreateTransactHistory(pointer *Q)
-{
+{ 
+    // allocates memory equal to the size of UserTransactionHistory to the head of transaction history from the heap.
     UserTransactionHistory *Head = (UserTransactionHistory *)malloc(sizeof(UserTransactionHistory));
     assert(Head != NULL);
     Head->next = NULL;
@@ -14,8 +15,10 @@ void InitCreateTransactHistory(pointer *Q)
     //Head->SenderUID = NULL;
 
     Q->H = Head;
-
+    
+    // allocates memory equal to the size of UserTransactionHistory to the tail of transaction history from the heap.
     UserTransactionHistory *Tail = (UserTransactionHistory *)malloc(sizeof(UserTransactionHistory));
+     assert(Tail != NULL);
     Tail->next = NULL;
     Tail->prev = NULL;
     Tail->AmountTransferred = -1;
@@ -25,18 +28,27 @@ void InitCreateTransactHistory(pointer *Q)
 }
 
 pointer *CreateUserTransactHistory()
-{
+{  
+    // allocates memory equal to size of head and tail nodes(i.e., struct pointer)
     pointer *Q = (pointer *)malloc(sizeof(pointer));
-
+    
+    // calls InitCreateTransactHistory() passing the pointer to the previous allocated memory.
+    // InitCreateTransactHistory updates Q by allocating memory for head and tail.
     InitCreateTransactHistory(Q);
     return Q;
 }
 
 void PrintUserTransactionHistory(char *UID)
-{
+{   
+    //Search user by UID and get a pointer to user(struct user).
     User *U = SearchUserByID(UID);
+    
+    //get pointer to user transaction history's head node.
     UserTransactionHistory *P = U->UTH->H;
-
+   
+    // since we are not storing transaction history in the head node so we will have to start from next node.
+    // so we will check the condition that head and head->next are not null.
+    // if they are null that means the user has not performed any transactions yet.
     if (P && P->next != NULL)
     {
         while (P && P->next != NULL)
@@ -48,32 +60,37 @@ void PrintUserTransactionHistory(char *UID)
                 printf("Amount Sent : %Lg\nReceiver UID : %s \nTransaction time : %s\n", P->AmountTransferred, P->ReceiverUID, P->TransactionTime);
         }
     }
-    else
+    else // no transactions performed by user.
         printf("This user hasn't performed any transactions.\n");
     printf("\n");
 }
 
 void PrintUserDetails(char *UID)
-{
+{   
+    //Search user by UID and get a pointer to user(struct user).
     User *U = SearchUserByID(UID);
+    
+    //if the pointer is null that means there is no such user with that user id.
     if (U == NULL)
     {
         printf("Invalid UserID\n");
         return;
     }
-    else
+    else // if userID is valid
     {
-        printf("\nUser ID : %s\n", U->UniqueID);
-        printf("Wallet Balance : %Lg\n", U->WalletBalance);
-        printf("User join time : %s\n", U->JoinDateTime);
+        printf("\nUser ID : %s\n", U->UniqueID); // prints UserID.
+        printf("Wallet Balance : %Lg\n", U->WalletBalance);// prints wallet balance
+        printf("User join time : %s\n", U->JoinDateTime); // prints join date time
 
         int ch;
-        printf("Do you wish to view the transaction history for this user? (1/0)\n");
+        printf("Enter 1 if you wish to view the transaction history for this user. Else enter 0.\n"); 
         scanf("%d", &ch);
 
         if (ch == 1)
         {
             printf("\nThe transaction history is as follows - \n\n");
+            
+            //call PrintUserTransactionHistory which prints transaction history for the user.
             PrintUserTransactionHistory(UID);
         }
     }
@@ -129,33 +146,33 @@ void push(pointer *Q, long double AmountTransferred, char *SenderUID, char *Rece
 int TransactionValidity(User *Sender, User *Receiver, long double AmountToBeTransferred)
 {
 
-    if (Sender == NULL)
+    if (Sender == NULL) //Sender with given UID doesnot exist
     {
         return 2;
     }
-    else if (Receiver == NULL)
+    else if (Receiver == NULL)//Receiver with given UID doesnot exist
     {
         return 3;
     }
-    else if (Sender == Receiver)
+    else if (Sender == Receiver)//If sender and receiver IDs are same(transaction with self is invalid).
     {
         return 6;
     }
-    else if (Sender->WalletBalance < AmountToBeTransferred)
+    else if (Sender->WalletBalance < AmountToBeTransferred)// Wallet balance of sender is lesser than the amount to be transferred.
     {
         return 4;
     }
 
-    else if (AmountToBeTransferred <= 0)
+    else if (AmountToBeTransferred <= 0) // amount of transaction is non positive
     {
         return 5;
     }
-    return 1;
+    return 1;//Everything is alright or transaction is valid.
 }
 
 void UpdateUserHistory(User *Sender, User *Receiver, char *SenderUID, char *ReceiverUID, long double AmountToBeTransferred)
 {
-    //update user transaction history
+    //push function updates user transaction history
     push(Sender->UTH, AmountToBeTransferred, SenderUID, ReceiverUID, 1);   // 1 -> amount was sent
     push(Receiver->UTH, AmountToBeTransferred, SenderUID, ReceiverUID, 0); // 0 -> amount was received
 
@@ -170,35 +187,33 @@ void UpdateBlockTransactionHistory(char *SenderUID, char *ReceiverUID, long doub
 {
     time_t t;
     time(&t);
-    if (TempTransactionArray == NULL)
+    if (TempTransactionArray == NULL) //if the pointer to array is null allocate memory for 50 transactions
     {
         TempTransactionArray = calloc(50, sizeof(BlockTransactionHistory));
     }
-
-    strcpy(TempTransactionArray[NumberofTempTransactions].SenderUID, SenderUID);
+    
+    //copy transaction details to the array.
+    strcpy(TempTransactionArray[NumberofTempTransactions].SenderUID, SenderUID); 
     strcpy(TempTransactionArray[NumberofTempTransactions].ReceiverUID, ReceiverUID);
     strcpy(TempTransactionArray[NumberofTempTransactions].TransactionTime, ctime(&t));
     TempTransactionArray[NumberofTempTransactions].AmountToBeTransferred = AmountToBeTransferred;
 
     NumberofTempTransactions++;
-    if (NumberofTempTransactions == 50)
+    if (NumberofTempTransactions == 50) //if the array is full
     {
-        CreateBlock();
+        CreateBlock(); 
         TempTransactionArray = NULL;
-        NumberofTempTransactions = 0;
+        NumberofTempTransactions = 0; //index = 0 again
     }
 }
 
 void Transact()
 {
-    //First search users (sender and receiver) with given IDs(T->SenderUID and T-> ReceiverUID)
-    //and get pointers to the users.
-    //long long SenderUID, ReceiverUID, AmountToBeTransferred;
-
     char SenderUID[40];
     char ReceiverUID[40];
     long double AmountToBeTransferred;
-
+    //long long SenderUID, ReceiverUID, AmountToBeTransferred;
+    
     printf("\nEnter the Sender User ID : ");
     scanf("%s", SenderUID);
     printf("\nEnter the Receiver User ID : ");
@@ -206,15 +221,17 @@ void Transact()
     printf("\nEnter the amount to be transferred : ");
     scanf("%Lg", &AmountToBeTransferred);
 
+    //First search users (sender and receiver) with given IDs(T->SenderUID and T-> ReceiverUID)
+    //and get pointers to the users.
     User *Sender = SearchUserByID(SenderUID);
     User *Receiver = SearchUserByID(ReceiverUID);
 
-    int isValid = TransactionValidity(Sender, Receiver, AmountToBeTransferred);
+    int isValid = TransactionValidity(Sender, Receiver, AmountToBeTransferred); //check transaction validity.
 
-    if (isValid == 1)
+    if (isValid == 1) // transaction is valid 
     {
-        UpdateBlockTransactionHistory(SenderUID, ReceiverUID, AmountToBeTransferred);
-        UpdateUserHistory(Sender, Receiver, SenderUID, ReceiverUID, AmountToBeTransferred);
+        UpdateBlockTransactionHistory(SenderUID, ReceiverUID, AmountToBeTransferred); //updates block transaction history array
+        UpdateUserHistory(Sender, Receiver, SenderUID, ReceiverUID, AmountToBeTransferred); // updates user transaction history
         printf("Transaction successful.\n");
     }
     else if (isValid == 2)
